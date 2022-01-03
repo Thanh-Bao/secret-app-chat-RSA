a5d32072b9aa779f8b7466e4229f0122e5e4941a
Bảo Bảo <44540022+Thanh-Bao@users.noreply.github.com>
2022-01-03 22:01:11 +0700

    update

--- pages/components/ChatItem/ChatItem.jsx
+++ pages/components/ChatItem/ChatItem.jsx
@@ -7,17 +7,26 @@ function ChatItem(props) {
 
     return (
         <div
-            id={styles.chatItemRow}
+            className={styles.chatItemRow}
             style={{ justifyContent: props.hihi ? "end" : "start" }}
         >
-            <span
-                style={{
-                    color: props.hihi ? "white" : null,
-                    backgroundColor : props.hihi? "blue" : null
-                }}
+            <div
+                className={styles.chatItem}
+                style={{ textAlign: props.hihi ? "end" : "start" }}
             >
-                dsrsgdsgf fafsag  dfg fg fdgdfg fgf gfgdf g fdgdfg dg gdfgd fdfgdfg dfg dfgdf dfg dffdgdf dfgd ff
-            </span>
+                <div
+                    className={styles.chatItemContent}
+                    style={{
+                        color: props.hihi ? "white" : null,
+                        backgroundColor: props.hihi ? "blue" : null
+                    }}
+                >
+                    dsrsgdsgf fafsag  dfg fg fdgdfg fgf gfgdf g fdgdfg dg gdfgd fdfgdfg dfg dfgdf dfg dffdgdf dfgd ff
+                </div>
+                <div className={styles.timestamp}>
+                    3r5322324
+                </div>
+            </div>
         </div>
     )
 }
