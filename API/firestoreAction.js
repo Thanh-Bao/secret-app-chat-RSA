8afc5a56ab9651a371325a872da25ac25f81cb3f
Bảo Bảo <44540022+Thanh-Bao@users.noreply.github.com>
2022-01-03 13:06:13 +0700

    test

--- API/firestoreAction.js
+++ API/firestoreAction.js
@@ -1,27 +1,27 @@
-import * as constants from '../pages/const'
-import { doc, getDoc, collection, setDoc } from 'firebase/firestore';
-import { db } from './firebase';
+// import * as constants from '../pages/const'
+// import { doc, getDoc, collection, setDoc } from 'firebase/firestore';
+// import { db } from './firebase';
 
-export const checkUsernameExists = async username => {
-    if (username === "") {
-        return false;
-    }
-    const docRef = doc(db, constants.COLLECTION_NAME, username);
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-        return true;
-    } else {
-        return false;
-    }
-}
+// export const checkUsernameExists = async username => {
+//     if (username === "") {
+//         return false;
+//     }
+//     const docRef = doc(db, constants.COLLECTION_NAME, username);
+//     const docSnap = await getDoc(docRef);
+//     if (docSnap.exists()) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
 
-export const addUser = async account => {
-    const { username, password, publicKey, recentConversation } = account;
-    try {
-        const userRef = collection(db, constants.COLLECTION_NAME);
-        await setDoc(doc(userRef, username), { password, publicKey, recentConversation });
-        return true;
-    } catch (err) {
-        return false;
-    }
-}
+// export const addUser = async account => {
+//     const { username, password, publicKey, recentConversation } = account;
+//     try {
+//         const userRef = collection(db, constants.COLLECTION_NAME);
+//         await setDoc(doc(userRef, username), { password, publicKey, recentConversation });
+//         return true;
+//     } catch (err) {
+//         return false;
+//     }
+// }
